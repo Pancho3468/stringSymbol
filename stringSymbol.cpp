b776c55c1045e0a0d25cbf05da123c04aa7160c7
Vitaly Panev <panevvitalij@gmail.com>
2023-10-16 22:21:15 +0300

    add string

--- stringSymbol.cpp
+++ stringSymbol.cpp
@@ -1,7 +1,14 @@
 ﻿
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string Animal = "giraffe";
+    std::cout << Animal << "\n";
+    std::cout << "In the word " << " " << Animal.length() << " Symbols" << "\n";
+    std::cout << "First character of the line = " << Animal [0] << "\n";
+    std::cout << "Last syllable of the line = " << Animal;
+    return 0;
+
 }
