37635ed59f7bd78446359e7c988f378a57e6b74e
Vitaly Panev <panevvitalij@gmail.com>
2023-10-17 10:47:03 +0300

    added first and last symbols

--- stringSymbol.cpp
+++ stringSymbol.cpp
@@ -7,8 +7,8 @@ int main()
     std::string Animal = "giraffe";
     std::cout << Animal << "\n";
     std::cout << "In the word " << " " << Animal.length() << " Symbols" << "\n";
-    std::cout << "First character of the line = " << Animal [0] << "\n";
-    std::cout << "Last syllable of the line = " << Animal;
+    std::cout << "First character of the line = " << Animal.front() << "\n";
+    std::cout << "Last syllable of the line = " << Animal.back() << "\n";
     return 0;
 
 }
